a1ab4b374d364562821a54e633528dda6873475a
kanatbekova telegey <telegey@MacBook-Air-kanatbekova.local>
2025-12-02 16:04:13 +0600

    catalog

---
+++ src/widgets/catalog/ui/Catalog.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import scss from './Catalog.module.scss';
+
+const Catalog: FC = () => {
+  return (
+    <section className={scss.Catalog}>
+      <div className="container">
+        <div className={scss.content}>
+          Catalog
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Catalog;
