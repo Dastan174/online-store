2d047d57dfe9ee7c29e112bfebc87e796e813c8c
Aseda2008 <asedanurdinova@gmail.com>
2025-12-03 23:31:51 +0600

    Finished testSection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TestSection } from "@/widgets/testSection/ui/TestSection";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
---
+++ src/entities/testOptionCard/index.tsx
@@ -0,0 +1,1 @@
+import {TestOptionCard} from "./ui/TestOptionCard"
