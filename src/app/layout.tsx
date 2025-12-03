6e93a2ea5677db90d9f415aff89b3c889ba8fe99
Akimal1 <akimturgunbaewv@gmail.com>
2025-12-03 20:47:44 +0600

    finished ProductardUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ProductCardUI from "@/entities/productCard/ui/ProductCardUI";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ProductCardUI />
         {children}
       </body>
     </html>
