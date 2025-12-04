e43e9239da8c17cc9d8ca68df881fceeeb20ccad
kalykov001 <kalykovsln@gmail.com>
2025-12-04 15:36:31 +0600

    i finish my task

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Hero from "@/widgets/hero/ui/Hero";
+import AboutUs from "@/widgets/aboutUs/ui/AboutUs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
