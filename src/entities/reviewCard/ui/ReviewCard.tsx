7435546ff896b71746cc9590a529b9bd58f8f168
ak1movumar <akimovumar2005@gmail.com>
2025-12-04 14:38:37 +0600

    questionsSection

--- src/entities/reviewCard/ui/ReviewCard.tsx
+++ src/entities/reviewCard/ui/ReviewCard.tsx
@@ -1,73 +1,39 @@
 import Image from "next/image";
 import scss from "./reviewCard.module.scss";
-import clientImg from "../../../../public/clientImg.svg";
-import clientImg2 from "../../../../public/clientImg2.svg";
-import clientImg3 from "../../../../public/clientImg3.svg";
 
-import yandexImg from "../../../../public/yandexMapsImg.svg";
+interface ReviewCardProps {
+  img: string;
+  clientName: string;
+  date: string;
+  comment: string;
+}
 
-export default function ReviewCard() {
+export default function ReviewCard({
+  img,
+  clientName,
+  date,
+  comment,
+}: ReviewCardProps) {
   return (
-    <>
-      <div className={scss.mainContainer}>
-        <div className={scss.header_content}>
-          <div className={scss.left_content}>
-            <Image src={clientImg} alt="client" width={70} height={70} />
-            <div className={scss.name_date}>
-              <h2>Serg</h2>
-              <span>27.07.2022</span>
-            </div>
-          </div>
-          <div className={scss.right_content}>
-            <Image src={yandexImg} alt="client" width={100} height={40} />
+    <div className={scss.mainContainer}>
+      <div className={scss.header_content}>
+        <div className={scss.left_content}>
+          <Image src={img} alt={clientName} width={70} height={70} />
+          <div className={scss.name_date}>
+            <h2>{clientName}</h2>
+            <span>{date}</span>
           </div>
         </div>
-        <p>
-          Долго думал где покупать арматуру на фундамент, по рекомендации друзей
-          заказывал на этой металлобазе. Менеджер Дмитрий все четко посчитал,
-          цена меня устроила. Доставка была в срок. Буду и дальше с ним
-          работать.
-        </p>
-      </div>
-      <div className={scss.mainContainer}>
-        <div className={scss.header_content}>
-          <div className={scss.left_content}>
-            <Image src={clientImg2} alt="client" width={70} height={70} />
-            <div className={scss.name_date}>
-              <h2>Квартира дом</h2>
-              <span>21.01.2022</span>
-            </div>
-          </div>
-          <div className={scss.right_content}>
-            <Image src={yandexImg} alt="client" width={100} height={40} />
-          </div>
+        <div className={scss.right_content}>
+          <Image
+            src="/yandexMapsImg.svg"
+            alt="yandex"
+            width={100}
+            height={40}
+          />
         </div>
-        <p>
-          Рекомендую! Прекрасная компания. Обращаюсь не в первый раз. Менеджеры
-          клиентоориентированы. Приятно, когда тебя и твои запросы понимают.
-          Всегда гуманные цены. Профлист качественный, снегозадержатели супер.
-          Спасибо!
-        </p>
       </div>
-      <div className={scss.mainContainer}>
-        <div className={scss.header_content}>
-          <div className={scss.left_content}>
-            <Image src={clientImg3} alt="client" width={70} height={70} />
-            <div className={scss.name_date}>
-              <h2>Вячеслав Е</h2>
-              <span>10.12.2022</span>
-            </div>
-          </div>
-          <div className={scss.right_content}>
-            <Image src={yandexImg} alt="client" width={100} height={40} />
-          </div>
-        </div>
-        <p>
-          Все чётко. Приняли заказ по телефону, пока ехал подготовили все
-          документы отправили на wats app точку чтобы не заблудился. Приехал,
-          оплатил, забрал 10 минут максимум.
-        </p>
-      </div>
-    </>
+      <p>{comment}</p>
+    </div>
   );
 }
--- src/widgets/ourClients/ui/OurClients.tsx
+++ src/widgets/ourClients/ui/OurClients.tsx
@@ -3,6 +3,33 @@ import scss from "./ourClients.module.scss";
 import { GoArrowLeft, GoArrowRight } from "react-icons/go";
 import ReviewCard from "@/entities/reviewCard/ui/ReviewCard";
 
+const reviews = [
+  {
+    id: 1,
+    img: "/clientImg.svg",
+    clientName: "Serg",
+    date: "27.07.2022",
+    comment:
+      "Долго думал где покупать арматуру на фундамент, по рекомендации друзей заказывал на этой металлобазе. Менеджер Дмитрий все четко посчитал, цена меня устроила. Доставка была в срок. Буду и дальше с ним работать.",
+  },
+  {
+    id: 2,
+    img: "/clientImg2.svg",
+    clientName: "Квартира дом",
+    date: "21.01.2022",
+    comment:
+      "Рекомендую! Прекрасная компания. Обращаюсь не в первый раз. Менеджеры клиентоориентированы. Приятно, когда тебя и твои запросы понимают. Всегда гуманные цены. Профлист качественный, снегозадержатели супер. Спасибо!",
+  },
+  {
+    id: 3,
+    img: "/clientImg3.svg",
+    clientName: "Вячеслав Е",
+    date: "10.12.2022",
+    comment:
+      "Все чётко. Приняли заказ по телефону, пока ехал подготовили все документы отправили на wats app точку чтобы не заблудился. Приехал, оплатил, забрал 10 минут максимум.",
+  },
+];
+
 const OurClients = () => {
   return (
     <div className={scss.container}>
@@ -21,7 +48,15 @@ const OurClients = () => {
             </span>
           </div>
           <div className={scss.cards}>
-            <ReviewCard />
+            {reviews.map((item) => (
+              <ReviewCard
+                key={item.id}
+                img={item.img}
+                clientName={item.clientName}
+                date={item.date}
+                comment={item.comment}
+              />
+            ))}
           </div>
         </div>
       </div>
