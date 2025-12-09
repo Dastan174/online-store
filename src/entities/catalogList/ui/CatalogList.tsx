bbe61d9dd6bf1586182fb6c8de782d33e71ab47a
kanatbekova telegey <telegey@MacBook-Air-kanatbekova.local>
2025-12-09 15:20:39 +0600

    finished dashboard

--- src/entities/catalogList/ui/CatalogList.tsx
+++ src/entities/catalogList/ui/CatalogList.tsx
@@ -1,18 +1,20 @@
 import { FC } from 'react'
 import scss from './CatalogList.module.scss'
-import ProductCardUI, { ProductCardProps } from '@/entities/productCard/ui/ProductCardUI'
 
+interface CatalogListProps {
+  items: string[]
+}
 
-export const CatalogList: FC<ProductCardProps> = () => {
-	return (
-				<ProductCardUI/>
-		// <section className={scss.list}>
-		// 	<div className={scss.grid}>
-		// 		{items.map((item, index) => (
-		// 			<div key={index} className={scss.card}>
-		// 			</div>
-		// 		))}
-		// 	</div>
-		// </section>
-	)
+export const CatalogList: FC<CatalogListProps> = ({ items }) => {
+  return (
+    <section className={scss.list}>
+      <div className={scss.grid}>
+        {items.map((item, index) => (
+          <div key={index} className={scss.card}>
+            {item}
+          </div>
+        ))}
+      </div>
+    </section>
+  )
 }
--- src/widgets/catalog/ui/CatalogSection.tsx
+++ src/widgets/catalog/ui/CatalogSection.tsx
@@ -1,38 +1,57 @@
-'use client'
+"use client";
 
-import { FC } from 'react'
-import scss from './Catalog.module.scss'
-import { CatalogList } from '@/entities/catalogList'
-import ProductCardUI from '@/entities/productCard/ui/ProductCardUI'
+import { FC } from "react";
+import scss from "./Catalog.module.scss";
+import { CatalogList } from "../../../entities/catalogList";
+import ProductCardUI from "../../../entities/productCard/ui/ProductCardUI";
+// import Button from '@/shared/ui/button'
+
 const CatalogSection: FC = () => {
-	const filters = ['Все', 'Металлочерепица', 'Профнастил', 'Фальцевая кровля']
+  const filters = ["Все", "Металлочерепица", "Профнастил", "Фальцевая кровля"];
+  const product = {
+    img: "https://lh6.googleusercontent.com/proxy/GjN501iwXUTQqlmhCzWe3aGqsZ16va1tpQFo5CeUblbo5jw7zQGKepcJaYcdnG0BEPPcCtXfY45-PWXeTqVkpMy5gyykblNGARDk5EzDK-4pWX8SxDuA5ntKSnFGzw",
+    productName: "Профнастил HC35",
+    brend: "Grandeline",
+    maxLength: "12",
+    minLength: "0,5",
+    price: 412,
+  };
 
-	return (
-		<section className={scss.section}>
-			<div className='container'>
-				<h1 className={scss.textCatalog}>КАТАЛОГ ТОВАРОВ</h1>
+  return (
+    <section className={scss.section}>
+      <div className="container">
+        <h1 className={scss.textCatalog}>КАТАЛОГ ТОВАРОВ</h1>
 
-				<div className={scss.filters}>
-					{filters.map((filter) => (
-						<button key={filter}> {filter}</button>
-					))}
-				</div>
+        <div className={scss.filters}>
+          {filters.map((filter) => (
+            <button key={filter}> {filter}</button>
+          ))}
+        </div>
 
-				<h2 className={scss.title}>Металлочерепица</h2>
-				<div className={scss.cards}>
-					<CatalogList />
-				</div>
-				<h2 className={scss.title}>Профнастил</h2>
-				<div className={scss.cards}>
-					<CatalogList />
-				</div>
-				<h2 className={scss.title}>Фальцевая кровля</h2>
-				<div className={scss.cards}>
-					<CatalogList />
-				</div>
-			</div>
-		</section>
-	)
-}
+        <h2 className={scss.title}>Металлочерепица</h2>
+        <div className={scss.cards}>
+          <ProductCardUI product={product} />
+          <ProductCardUI product={product} />
+          <ProductCardUI product={product} />
+          <ProductCardUI product={product} />
+        </div>
+        <h2 className={scss.title}>Профнастил</h2>
+        <div className={scss.cards}>
+          <ProductCardUI product={product} />
+          <ProductCardUI product={product} />
+          <ProductCardUI product={product} />
+          <ProductCardUI product={product} />
+        </div>
+        <h2 className={scss.title}>Фальцевая кровля</h2>
+        <div className={scss.cards}>
+          <ProductCardUI product={product} />
+          <ProductCardUI product={product} />
+          <ProductCardUI product={product} />
+          <ProductCardUI product={product} />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default CatalogSection
+export default CatalogSection;
