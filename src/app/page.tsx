0ef5fb6e70bcc30183b3e3ddee162eadd8ae00cf
Akimal1 <akimturgunbaewv@gmail.com>
2025-12-11 12:18:21 +0600

    adminCorecct

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import ProductForm from "../entities/productForm/ui/ProductForm";
 import AboutUs from "../widgets/aboutUs/ui/AboutUs";
 import CatalogSection from "../widgets/catalog/ui/CatalogSection";
-import { Footer } from "../widgets/footer";
-import { Header } from "../widgets/header";
+import Footer from "../widgets/footer/ui/Footer";
+import Header from "../widgets/header/ui/Header";
 import Hero from "../widgets/hero/ui/Hero";
-import Orders from "../widgets/orders/ui/Orders";
 import OurClients from "../widgets/ourClients/ui/OurClients";
-import Products from "../widgets/products/ui/Products";
 import QuestionsSection from "../widgets/questionsSection/ui/QuestionsSection";
-import Sidebar from "../widgets/sidebar/ui/Sidebar";
 import { TestSection } from "../widgets/testSection/ui/TestSection";
 
 const page = () => {
@@ -26,7 +22,6 @@ const page = () => {
         <OurClients />
       </div>
       <Footer />
-      <Orders/>
     </div>
   );
 };
